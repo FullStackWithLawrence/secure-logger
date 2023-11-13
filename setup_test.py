3498447f7b8ba3ff5264147aec9b68129c642a2a
lpm0073 <lpm0073@gmail.com>
2023-11-13 16:09:46 -0600

    test: refactor

--- setup_test.py
+++ setup_test.py
@@ -6,13 +6,11 @@ import unittest
 class TestSetup(unittest.TestCase):
     """Test setup.py."""
 
-    def test_setup_syntax():
+    def test_setup_syntax(self):
         """Test setup.py syntax."""
-        result = subprocess.run(["python", "setup.py"], capture_output=True)
-        assert (
-            result.returncode == 0
-        ), f"setup.py failed with output:\n{result.stdout.decode()}\n{result.stderr.decode()}"
-        assert "MockDecoratedClass" in result.stdout.decode(), "Expected 'MockDecoratedClass' in output"
+        result = subprocess.run(["python", "setup.py", "check"], capture_output=True, text=True)
+        assert result.returncode == 0, f"setup.py failed with output:\n{result.stdout}\n{result.stderr}"
+        assert not result.stderr, "Expected no error output"
 
 
 if __name__ == "__main__":
