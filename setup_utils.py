1ac6246f4840457a62d6a7a6ccf03065c28643c7
lpm0073 <lpm0073@gmail.com>
2023-11-14 08:53:16 -0600

    fix: add regex expressions to correctly evaluate all possible values of __version__.py

--- setup_utils.py
+++ setup_utils.py
@@ -3,6 +3,7 @@
 # pylint: disable=open-builtin
 import io
 import os
+import re
 from typing import Dict
 
 HERE = os.path.abspath(os.path.dirname(__file__))
@@ -26,9 +27,20 @@ def get_semantic_version() -> str:
     """
     Return the semantic version number.
 
+    Example valid values of __version__.py are:
+    0.1.17
+    0.1.17-next.1
+    0.1.17-next.2
+    0.1.17-next.123456
+    0.1.17-next-major.1
+    0.1.17-next-major.2
+    0.1.17-next-major.123456
+
     Note:
     - pypi does not allow semantic version numbers to contain a dash.
     - pypi does not allow semantic version numbers to contain a 'v' prefix.
     - pypi does not allow semantic version numbers to contain a 'next' suffix.
     """
-    return VERSION["__version__"].replace("-next.", "a")
+    version = VERSION["__version__"]
+    version = re.sub(r"-next\.\d+", "", version)
+    return re.sub(r"-next-major\.\d+", "", version)
